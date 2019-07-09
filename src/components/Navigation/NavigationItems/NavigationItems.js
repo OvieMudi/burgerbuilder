b6025ff5446729ee88a30a2a55bc619384487735
Ovie Mudi <oviisaacs@gmail.com>
2019-07-09 23:06:51 +0100

    feat(toolbar): add toolbar with navigation links

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavigationItem from './NavigationItem/NavigationItem';
+import classes from './NavigationItems.module.css';
+
+const NavigationItems = props => {
+  return (
+    <ul className={classes.NavigationItems}>
+      <NavigationItem link={'/'} active>
+        Burger Builder
+      </NavigationItem>
+      <NavigationItem link={'/'}>Checkout</NavigationItem>
+    </ul>
+  );
+};
+
+export default NavigationItems;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const Toolbar = props => {
   return (
     <header className={classes.Toolbar}>
       <div>Menu</div>
       <Logo />
-      <nav>links</nav>
+      <nav>
+        <NavigationItems />
+      </nav>
     </header>
   );
 };
